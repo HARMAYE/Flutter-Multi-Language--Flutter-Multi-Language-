122585bede478bbe3940496b4a0121cd8236b2dc
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2020-05-27 12:24:29 +0000

    * fix typo

--- algo/sma.go
+++ algo/sma.go
@@ -8,4 +8,4 @@ import (
 )
 
 /*
-http://www.investopedia.com/articles/active-trading/012815/top-technical-indicator
+http://www.investopedia.com/articles/active-trading/012815/top-technical-indicators-scalping-tra
