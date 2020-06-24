661ed49771ee925e6cd9a3f5a0a8b95116886e04
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2020-06-24 22:57:16 +0000

    Update aliases.zsh

--- algo/sma.go
+++ algo/sma.go
@@ -9,4 +9,4 @@ import (
 
 /*
 http://www.investopedia.com/articles/active-trading/012815/top-technical-indicators-scalping-trading-strategy.asp
-http://www.investopedia.com/articles/acti
+http://www.investopedia.com/articles/active-tradi
