a42115938ee134aa413b824b602c54de74c7b678
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2020-07-13 07:33:44 +0000

    initial version commit

--- algo/sma.go
+++ algo/sma.go
@@ -10,4 +10,4 @@ import (
 /*
 http://www.investopedia.com/articles/active-trading/012815/top-technical-indicators-scalping-trading-strategy.asp
 http://www.investopedia.com/articles/active-trading/010116/perfect-moving-averages-day-trading.asp
-http://www.investopedia.co
+http://www.investopedia.com/article
