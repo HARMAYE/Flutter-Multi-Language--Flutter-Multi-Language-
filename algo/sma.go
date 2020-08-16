c78a55dbf5457499b22e50dc03439d59e2a5789a
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2020-08-16 17:18:55 +0000

    Bug fixed:

--- algo/sma.go
+++ algo/sma.go
@@ -12,3 +12,6 @@ http://www.investopedia.com/articles/active-trading/012815/top-technical-indicat
 http://www.investopedia.com/articles/active-trading/010116/perfect-moving-averages-day-trading.asp
 http://www.investopedia.com/articles/active-trading/012815/top-technical-indicators-scalping-trading-strategy.asp
 http://www.day-trading-stocks.org/moving-average-crossover.html
+ */
+
+type
