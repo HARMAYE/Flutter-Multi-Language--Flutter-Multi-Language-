bb768492a88551a4126b7b6de6a764178d33ff48
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2020-10-15 07:11:25 +0000

    correct order

--- algo/sma.go
+++ algo/sma.go
@@ -33,4 +33,4 @@ func (o *SimpleMovingAvg) Push(x int) {
 func (o *SimpleMovingAvg) Value() int {
 	var sum int = 0
 	var currPeriod = 0
-	for e := o.data.Front(); e != nil && currPeriod <= o.p
+	for e := o.data.Front(); e != nil && currPeriod <= o.period; e = e.Ne
