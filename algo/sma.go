b9e2834f10ac938f82eb29718a64b0b008c31bb2
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2020-11-03 09:35:47 +0000

    :peach:

--- algo/sma.go
+++ algo/sma.go
@@ -43,4 +43,4 @@ func (o *SimpleMovingAvg) Value() int {
 func (o *SimpleMovingAvg) String() string {
 	return fmt.Sprintf(
 		"SimpleMovingAvg(Period:%d Value:%d Trend:%d Slice:%+v)",
-		o.period, o.Val
+		o.period, o.Value(), o.Tren
