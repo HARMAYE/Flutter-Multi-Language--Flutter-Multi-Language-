a0a0788b98210fe0fc308dd1cad4e05863b843c0
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2021-03-04 22:00:36 +0000

    update lists

--- algo/sma.go
+++ algo/sma.go
@@ -127,4 +127,4 @@ func (o *SmaTriple) Push(x int) {
 func (o *SmaTriple) Signal() int {
 	o.mutex.RLock()
 	defer o.mutex.RUnlock()
-	if o.slow.Trend() == stockfig
+	if o.slow.Trend() == stockfighter.Tre
