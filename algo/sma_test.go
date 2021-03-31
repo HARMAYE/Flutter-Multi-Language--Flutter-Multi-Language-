35279eb1a6a8a63d6828b0f0f4d7c482c0a0a1b4
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2021-03-31 17:55:51 +0000

    Delete changelog

--- algo/sma_test.go
+++ algo/sma_test.go
@@ -8,3 +8,15 @@ import (
 )
 
 var testPeriod int = 5
+
+func TestSimpleMovingAvg(t *testing.T) {
+	sma := InitSimpleMovingAvg(testPeriod)
+	if sma.Value() != 0 {
+		t.Error("bad value", sma.Value())
+	}
+}
+
+func TestSimpleMovingAvg_Push(t *testing.T) {
+	sma := InitSimpleMovingAvg(testPeriod)
+	for i := 1; i <= testPeriod; i++ {
+		sma.Push(i)
