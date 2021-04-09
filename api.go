2fc584825727331c0065d6a1f4199452c69027d8
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2021-04-09 23:08:37 +0000

    Added requirements file.

--- api.go
+++ api.go
@@ -14,3 +14,17 @@ import (
 /*
 Web Sockets: https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/08.2.html
 */
+
+var (
+	PRINT_JSON_RESPONSE = false
+)
+
+type Api struct {
+	Config *config
+}
+
+func InitApi(config *config) *Api {
+	return &Api{
+		Config: config,
+	}
+}
