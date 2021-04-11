60159655bc337119b869c0629ddbb79fe2f5192c
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2021-04-11 15:29:12 +0000

    Remove build folder

--- api.go
+++ api.go
@@ -47,3 +47,20 @@ func (s *Api) HeartBeat() (*HeartBeat, error) {
 
 func (s *Api) VenueHeartBeat(venue string) (*VenueHeartBeat, error) {
 	buffer, err := s.GetRequest(fmt.Sprintf("ob/api/venues/%s/heartbeat", venue))
+	if err != nil {
+		return nil, err
+	}
+
+	var value *VenueHeartBeat
+	jsonErr := json.Unmarshal(buffer, &value)
+	if jsonErr == nil {
+		return value, nil
+	}
+
+	return nil, jsonErr
+}
+
+func (s *Api) VenueStocks(venue string) (*VenueStocks, error) {
+	buffer, err := s.GetRequest(fmt.Sprintf("ob/api/venues/%s/stocks", venue))
+	if err != nil {
+		return nil, err
