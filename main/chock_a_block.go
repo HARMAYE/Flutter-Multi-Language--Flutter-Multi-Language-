de3eab5e6209be28e13ee59c0a78289f5efc755f
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2021-06-02 11:40:27 +0000

    Linux 3.10.99

--- main/chock_a_block.go
+++ main/chock_a_block.go
@@ -13,3 +13,14 @@ var (
 	venue = "TJMEX"
 	symbol = "MBL"
 	account = "KEG25461931"
+
+	config = stockfighter.InitConfig(".env.yml", account)
+	api = stockfighter.InitApi(config)
+	random = rand.New(rand.NewSource(time.Now().Unix()))
+)
+
+func main() {
+	var currShares = 25800
+	targetShares := 100000
+	maxShareStep := 30000
+	maxAskStep := 100
