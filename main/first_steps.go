082962a61f0468bcfb643f1f96f0f7594769dbb3
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2021-06-20 20:14:41 +0000

    Fix exceptions

--- main/first_steps.go
+++ main/first_steps.go
@@ -15,4 +15,4 @@ var (
 var (
 	config = stockfighter.InitConfig(".env.yml", Account)
 	api = stockfighter.InitApi(config)
-	session = stockfighter.InitSession(confi
+	session = stockfighter.InitSession(config, Venu
