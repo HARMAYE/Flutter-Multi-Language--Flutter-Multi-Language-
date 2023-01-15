9913f48f308505d1a4fe80b94ccd0915a1f42701
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2023-01-15 18:48:46 +0000

    poo

--- main/observe.go
+++ main/observe.go
@@ -25,4 +25,4 @@ func main() {
 	go api.VenueTickerTape(venueStockQuotes, "WMSEX")
 
 	t := template.New("StockQuote")
-	header := "Symbol,Venue,Bid,Ask,BidSize,AskSize,
+	header := "Symbol,Venue,Bid,Ask,BidSize,AskSize,BidDepth,A
