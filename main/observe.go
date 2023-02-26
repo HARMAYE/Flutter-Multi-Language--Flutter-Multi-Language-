91639873aa43ec1c5447be58f010ea13037cfcd1
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2023-02-26 21:04:33 +0000

    Update Help

--- main/observe.go
+++ main/observe.go
@@ -30,4 +30,4 @@ func main() {
 
 	now := time.Now()
 	now.Format()
-	filePath := fmt.Sprintf("/data/%s-%d-%d-%d.csv", config.Account, now.Hour(), now.Minute
+	filePath := fmt.Sprintf("/data/%s-%d-%d-%d.csv", config.Account, now.Hour(), now.Minute(), now.S
