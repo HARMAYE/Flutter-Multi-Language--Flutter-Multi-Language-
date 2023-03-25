fa977433ff7b8f52ffda47a3176d800772df313b
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2023-03-25 12:59:27 +0000

    updated pybasicbayes

--- main/sell_side.go
+++ main/sell_side.go
@@ -1,2 +1,20 @@
 
 package main
+
+import (
+	"fmt"
+	"github.com/cevaris/stockfighter"
+	"github.com/cevaris/stockfighter/algo"
+	"time"
+)
+
+var (
+	Account = "ATE44401158"
+	Venue = "LVBTEX"
+	Symbol = "RPM"
+)
+
+var (
+	config = stockfighter.InitConfig(".env.yml", Account)
+	api = stockfighter.InitApi(config)
+	session = stockfighter.InitSession(config, Venue)
