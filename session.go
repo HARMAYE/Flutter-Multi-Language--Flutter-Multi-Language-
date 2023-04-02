994aeb68f6d7c8d699e2832b7055cfde006df8e2
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2023-04-02 21:36:01 +0000

    Resume

--- session.go
+++ session.go
@@ -9,3 +9,14 @@ import (
 
 type Session struct {
 	Cash          int
+	NAV           int
+	Position      int
+	TotalPosition int
+	Venue         string
+	api           *Api
+	config        *config
+	mutex         *sync.RWMutex
+	LatestQuote   *StockQuote
+	quoteChan     chan *StockQuote
+	fillChan      chan *Execution
+}
