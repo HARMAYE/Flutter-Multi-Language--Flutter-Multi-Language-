381540a18ccd8c857a6703623b02631e2fde9dc8
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2023-04-16 18:12:33 +0000

    Calculator

--- session_test.go
+++ session_test.go
@@ -17,3 +17,17 @@ func TestInitSession(t *testing.T) {
 	}
 	if session.NAV != 0 {
 		t.Error("invalid nav", session.NAV)
+	}
+}
+
+func TestSessionUpdate(t *testing.T) {
+	session := InitSession(&config{}, testVenue)
+	session.LatestQuote = &StockQuote{Last: 100}
+	session.Update(&StockOrderAccountStatus{
+		Ok: true,
+		Venue: testVenue,
+		Orders: []*StockOrder{
+			&StockOrder{
+				Ok: true,
+				Direction: DirectionBuy,
+				Fills: []*Fill{
