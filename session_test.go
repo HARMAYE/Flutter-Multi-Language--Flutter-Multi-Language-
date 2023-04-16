7cada298546b9f42d7e9c5d58b6c4a328fc0f117
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2023-04-16 16:33:55 +0000

    Uncomment

--- session_test.go
+++ session_test.go
@@ -8,3 +8,12 @@ import (
 var testVenue = "TestVenue"
 
 func TestInitSession(t *testing.T) {
+	session := InitSession(&config{}, testVenue)
+	if session.Position != 0 {
+		t.Error("invalid position", session.Position)
+	}
+	if session.Cash != 0 {
+		t.Error("invalid cash", session.Cash)
+	}
+	if session.NAV != 0 {
+		t.Error("invalid nav", session.NAV)
