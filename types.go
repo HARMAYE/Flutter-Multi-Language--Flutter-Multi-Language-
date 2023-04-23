368223560cebb56aac2a85fd94b2db7b77b98b79
HARMAYE <110253019+HARMAYE@users.noreply.github.com>
2023-04-23 20:12:46 +0000

    Changed method name

--- types.go
+++ types.go
@@ -4,3 +4,12 @@ package stockfighter
 import (
 	"time"
 	"fmt"
+	"encoding/json"
+)
+
+const (
+	DirectionBuy string = "buy"
+	DirectionSell string = "sell"
+
+	OrderLimit string = "limit"
+	OrderMarket string = "market"
